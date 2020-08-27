c8ddedde790f09226215bb1983fc090128159583
EvgDeGa <dorokhovjug@gmail.com>
2020-08-27 23:48:54 +0700

    room preview 1/2

---
+++ src/rate_button/rate_button.js
@@ -0,0 +1,25 @@
+import './rate_button.scss';
+
+window.onload = function () {
+    let img = document.getElementsByClassName('star')
+
+    for(let i = 0; i < img.length; i++){
+        img[i].addEventListener("click", () => clicked(i, img), false)
+
+    }
+
+    function clicked(i, img) {
+        console.log('ss')
+        console.log(i)
+        for(let j = 0; j < i + 1; j++){
+            $(img[j]).children('path').attr('d', 'M10.2637 15.749L4.07617 19.499L5.7168 12.4678L0.279297 7.7334L7.45117 7.12402L10.2637 0.514648L13.0762 7.12402L20.248 7.7334L14.8105 12.4678L16.4512 19.499L10.2637 15.749Z" fill="url(#paint0_linear)');
+            console.log('s12s')
+        }
+        for(let j = 4; j > i; j--){
+            $(img[j]).children('path').attr('d', 'M10.2637 13.9209L14.0137 16.1709L13.0293 11.9053L16.3574 8.99902L11.9512 8.62402L10.2637 4.59277L8.57617 8.62402L4.16992 8.99902L7.49805 11.9053L6.51367 16.1709L10.2637 13.9209ZM20.248 7.7334L14.8105 12.4678L16.4512 19.499L10.2637 15.749L4.07617 19.499L5.7168 12.4678L0.279297 7.7334L7.45117 7.12402L10.2637 0.514648L13.0762 7.12402L20.248 7.7334Z" fill="url(#paint0_linear)');
+            console.log('s12s')
+        }
+    }
+
+
+}
--- src/room_preview/room_preview.js
+++ src/room_preview/room_preview.js
@@ -1,1 +1,29 @@
 import './room_preview.scss';
+
+window.onload = function () {
+    let img = document.getElementsByClassName('circle')
+    let left = document.getElementsByClassName('left')
+    let right = document.getElementsByClassName('right')
+    let i = {Value: 0}
+
+    left[0].addEventListener("click", () => clicked(-1, img), false)
+    right[0].addEventListener("click", () => clicked(1, img), false)
+
+
+    $(img[0]).children('g').html('<ellipse cx="5" cy="5" rx="3.75" ry="3.74693" fill="white"/>\n')
+
+    function clicked(buf, img) {
+
+        i.Value += buf
+        if(i.Value > 3) i.Value = 0
+        if(i.Value < 0) i.Value = 3
+
+        for(let j = 0; j < 4; j++){
+            $(img[j]).children('g').html('<ellipse cx="5" cy="5" rx="3.25" ry="3.24693" stroke-width="1" stroke="white"/>');
+        }
+
+        $(img[i.Value]).children('g').html('<ellipse cx="5" cy="5" rx="3.75" ry="3.74693" fill="white"/>\n')
+    }
+
+
+}
